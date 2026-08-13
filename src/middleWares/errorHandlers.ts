6b0b2944dd7a217214b17ab1c36e70833abcb8ca
Jim-R10 <jim.ramananarivo10@gmail.com>
2026-08-13 10:45:59 +0300

    package src with endpoint students

---
+++ src/middleWares/errorHandlers.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from 'express';
+
+
+export function errorHandler(
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const status = err.status || 500;
+  const message = err.message || 'Erreur interne du serveur';
+
+  console.log('Erreur attrapée :', message);
+
+  res.status(status).json({ error: message });
+}
