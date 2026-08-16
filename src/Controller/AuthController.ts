13888e3e89b65765a0469b444f7140dd56acd183
Jim-R10 <jim.ramananarivo10@gmail.com>
2026-08-16 19:07:14 +0300

    feat: add AuthController

---
+++ src/Controller/AuthController.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from 'express';
+import { AuthService } from '../Service/AuthService';
+
+async function register(req: Request, res: Response, next: NextFunction) {
+  try {
+    const resultat = await AuthService.register(req.body);
+    res.status(201).json(resultat);
+  } catch (erreur) {
+    next(erreur);
+  }
+}
+
+async function login(req: Request, res: Response, next: NextFunction) {
+  try {
+    const resultat = await AuthService.login(req.body);
+    res.status(200).json(resultat);
+  } catch (erreur) {
+    next(erreur);
+  }
+}
+
+export const AuthController = {
+  register: register,
+  login: login
+};
