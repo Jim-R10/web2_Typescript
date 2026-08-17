38c49a6ee304b6ca62e86baafdf657a86b104e80
Jim-R10 <jim.ramananarivo10@gmail.com>
2026-08-17 17:50:51 +0300

    refactor: add cors in index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,28 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
+import cors from 'cors';
 import authController from './Controller/AuthController';
 import studentController from './Controller/studentController';
 import { errorHandler } from './middleWares/errorHandlers';
 
 const app = express();
 
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
-app.use('/auth', authController);        
-app.use('/students', studentController); 
+app.use('/auth', authController);
+app.use('/students', studentController);
 
 app.use(errorHandler);
 
-const PORT = 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 app.listen(PORT, function () {
   console.log('Serveur lancé sur http://localhost:' + PORT);
